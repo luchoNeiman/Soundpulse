092546cb53e5bd31de40a005c0f1f5f2b440d2e9
luchoNeiman <neimanlucho@gmail.com>
2026-04-01 02:32:12 -0300

    creacion de rutas

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: () => import('../views/AdminView.vue')
+    },
+    {
+      path: '/investigacion',
+      name: 'investigacion',
+      component: () => import('../views/InvestigacionView.vue')
+    }
+  ],
 })
 
 export default router
